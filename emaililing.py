931b0f1ad4b858764d42ae3b26ba603c2a5ef049
AlexVic1 <105377287+AlexVic1@users.noreply.github.com>
2023-05-03 22:03:25 +0300

    first commit

---
+++ emaililing.py
@@ -0,0 +1,31 @@
+from email.message import EmailMessage
+import imghdr
+import smtplib
+
+PASSWORD = "" ""# You need to turn on two step verification.
+SENDER = "" # Put here the email that you would like to send it.
+RECEIVER = "" # Put here the email that you would like to receiv it.
+
+def send_email(image_path):
+    print("send_email function started")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer")
+    print("send_email function ended")
+    
+    
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+#test
+if __name__ == "__main__":
+    send_email(image_path="")
